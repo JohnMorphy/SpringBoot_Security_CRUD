b07b192351fbc0e7f4d661d59335dfa81f9f981f
Jan <jankaiser.gdynia@gmail.com>
2023-12-09 19:12:05 +0100

    Built Project

---
+++ Lab6_SpringBooks/src/main/java/ksi/springbooks/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package ksi.springbooks.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	http
+		.csrf(Customizer.withDefaults())
+		.authorizeHttpRequests(authorize -> authorize
+		.requestMatchers("/new_book", "/edit_book/**", "/delete_book/**").hasRole("BOOK_MANAGER")
+		.requestMatchers("/new_book", "/edit_book/**", "/delete_book/**",
+				"/new_category", "/edit_category/**", "/delete_category/**",
+				"new_publisher", "/edit_publisher/**", "/delete_publisher/**").hasRole("ADMIN")
+		.anyRequest().permitAll()
+		)
+	.formLogin(form -> form
+			 .loginPage("/login")
+			 .loginProcessingUrl("/login")
+			 .defaultSuccessUrl("/")
+			 .permitAll())
+	.logout(logout -> logout
+			.logoutUrl("/logout")
+		    .logoutSuccessUrl("/logout_message")
+		);
+	
+	return http.build();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+	 return new BCryptPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+		String result = encoder.encode("passu1");
+		System.out.println(result);
+	}
+	
+	
+}
